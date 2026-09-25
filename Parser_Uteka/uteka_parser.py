74e94721e56e05673e0f3de2f8f49e0aa607f606
Poggg <ivansmi.go@yandex.ru>
2026-09-26 00:05:44 +0300

    Релиз

--- Parser_Uteka/uteka_parser.py
+++ Parser_Uteka/uteka_parser.py
@@ -77,7 +77,7 @@ def get_uteka_data(map_url, channel='chrome', city='Москва'):
         for card in cards:
             title = card.locator('._size-tablet-300').inner_text()
             price = card.locator('.ui-price_theme_primary').inner_text()
-            address = card.locator('._size-tablet-200').inner_text()
+            address = card.locator('[data-test="address"]').inner_text()
             first_result.append([price, title, address])
 
         page.on('response', handle_response)
